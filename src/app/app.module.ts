ad8a0fb8b1238b711156be6a01bde20150c00c16
Rufo138 <rufino_138@hotmail.com>
2021-10-03 14:23:04 -0300

    nuevo componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeroesModule } from './heroes/heroes.module';
 import { ContadorModule } from './contadores/contador.module';
+import { DbzModule } from './dbz/dbz.module';
 @NgModule({
   declarations: [
     AppComponent,
@@ -11,6 +12,7 @@ import { ContadorModule } from './contadores/contador.module';
     BrowserModule,
     HeroesModule,
     ContadorModule,
+    DbzModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
