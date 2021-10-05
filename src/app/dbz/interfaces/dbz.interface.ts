cb66a07cc1ee91eceb7936cab9a0a7d3868db2b1
Rufo138 <rufino_138@hotmail.com>
2021-10-05 04:55:20 -0300

    nuevo comit

---
+++ src/app/dbz/interfaces/dbz.interface.ts
@@ -0,0 +1,8 @@
+export interface Personaje {
+    nombre:string;
+    poder:number;
+  }
+export interface nuevo{
+  nombre:string;
+  poder:number;
+}
--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,12 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component} from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interface';
+import { DbzService } from '../services/dbz.service';
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html'
 })
 export class MainPageComponent{
-  agregar(event:any){
-    event.preventDefault();
-    console.log("tu vieja");
+  
+  nuevo:Personaje={
+    nombre:'',
+    poder:0
+  }
+  constructor(){
   }
 }
